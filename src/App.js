5dbf66723d5858b70b9100af302943070c5b9ccf
HyeJun <solar3070@naver.com>
2022-05-08 18:42:11 +0900

    App에 todos 상태 사용, TodoHead 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { createGlobalStyle } from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoHead from "./components/TodoHead";
@@ -5,11 +6,34 @@ import TodoList from "./components/TodoList";
 import TodoCreate from "./components/TodoCreate";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "프로젝트 생성하기",
+      done: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링 하기",
+      done: true,
+    },
+    {
+      id: 3,
+      text: "Context 만들기",
+      done: false,
+    },
+    {
+      id: 4,
+      text: "기능 구현하기",
+      done: false,
+    },
+  ]);
+
   return (
     <>
       <GlobalStyle />
       <TodoTemplate>
-        <TodoHead />
+        <TodoHead todos={todos} />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
